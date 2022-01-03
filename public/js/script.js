ee04ee39be635037e570e7436486f59f5c6f69ec
Suraj325 <suraj1503kain@gmail.com>
2022-01-03 22:22:12 +0530

    added

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,12 @@
-document.querySelector(".minus-btn").setAttribute("disabled", "disabled");
+var x = document.getElementsByClassName("minus-btn")[0];
+x.setAttribute("disabled", "disabled");
+
+var y = document.getElementsByClassName("minus-btn")[1];
+y.setAttribute("disabled", "disabled");
+
+var z = document.getElementsByClassName("minus-btn")[2];
+z.setAttribute("disabled", "disabled");
+
 var valueCount;
 var price = document.getElementById("price").value;
 function priceTotal() {
@@ -6,21 +14,60 @@ function priceTotal() {
   document.getElementById("price").value = total;
 }
 function plus() {
-  valueCount = document.getElementById("quantity").value;
+  valueCount = document.getElementById("quantity1").value;
   valueCount++;
-  document.getElementById("quantity").value = valueCount;
+  document.getElementById("quantity1").value = valueCount;
   if (valueCount > 1) {
-    document.querySelector(".minus-btn").removeAttribute("disabled");
-    document.querySelector(".minus-btn").classList.remove("disabled");
+    x.removeAttribute("disabled");
+    x.classList.remove("disabled");
   }
   priceTotal();
 }
 function minus() {
-  valueCount = document.getElementById("quantity").value;
+  valueCount = document.getElementById("quantity1").value;
   valueCount--;
-  document.getElementById("quantity").value = valueCount;
+  document.getElementById("quantity1").value = valueCount;
   if (valueCount == 1) {
-    document.querySelector(".minus-btn").setAttribute("disabled", "disabled");
+    x.setAttribute("disabled", "disabled");
+  }
+  priceTotal();
+}
+
+function plus1() {
+  valueCount = document.getElementById("quantity2").value;
+  valueCount++;
+  document.getElementById("quantity2").value = valueCount;
+  if (valueCount > 1) {
+    y.removeAttribute("disabled");
+    y.classList.remove("disabled");
+  }
+  priceTotal();
+}
+function minus1() {
+  valueCount = document.getElementById("quantity2").value;
+  valueCount--;
+  document.getElementById("quantity2").value = valueCount;
+  if (valueCount == 1) {
+    y.setAttribute("disabled", "disabled");
+  }
+  priceTotal();
+}
+function plus2() {
+  valueCount = document.getElementById("quantity3").value;
+  valueCount++;
+  document.getElementById("quantity3").value = valueCount;
+  if (valueCount > 1) {
+    z.removeAttribute("disabled");
+    z.classList.remove("disabled");
+  }
+  priceTotal();
+}
+function minus2() {
+  valueCount = document.getElementById("quantity3").value;
+  valueCount--;
+  document.getElementById("quantity3").value = valueCount;
+  if (valueCount == 1) {
+    z.setAttribute("disabled", "disabled");
   }
   priceTotal();
 }
